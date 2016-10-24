739d5e055d0d7c7195431036ac4d7f294a25dfc5
/ <i411522@163.com>
2016-10-24 22:17:34 +0800

    aaa

--- examples/todos-with-undo/src/actions/index.js
+++ examples/todos-with-undo/src/actions/index.js
@@ -1,16 +1,21 @@
 let nextTodoId = 0
+//添加TODO
 export const addTodo = (text) => ({
   type: 'ADD_TODO',
   id: nextTodoId++,
   text
 })
 
+//设置过滤属性
 export const setVisibilityFilter = (filter) => ({
   type: 'SET_VISIBILITY_FILTER',
   filter
 })
 
+//toggleTodo
 export const toggleTodo = (id) => ({
   type: 'TOGGLE_TODO',
   id
 })
+
+//试试
